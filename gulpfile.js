52f98b3a941798b1dbd4e4a0ee2bf40bc6c89680
Farhan Ghazali <farhan_ghazali@yahoo.com>
2019-04-24 21:53:52 +0800

    Use gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { dest, watch, src, series } = require('gulp')
+const ts = require('gulp-typescript')
+const tap = require('gulp-tap')
+const path = require('path')
 const fs = require('fs')
 const glob = require('glob')
 const shortid = require('shortid')
 const arrToObj = require('array-to-object')
-const ts = require('gulp-typescript')
 
 const tsProject = ts.createProject('tsconfig.json')
 
@@ -18,12 +20,35 @@ function buildTs () {
     .pipe(dest('build'))
 }
 
+function buildHtml () {
+  return src('src/**/*.html')
+    .pipe(dest('build'))
+}
+
+function mergeSvelte () {
+  return src('src/**/*.ts')
+    .pipe(tsProject())
+    .pipe(tap(function(file) {
+      const svelteHtmlPath = file.path.replace('build', 'src').replace('.js', '.html')
+      if (fs.existsSync(svelteHtmlPath)) {
+        file.contents = Buffer.concat([
+          fs.readFileSync(svelteHtmlPath),
+          Buffer.from(`\r\n\r\n<script>\r\n`),
+          file.contents,
+          Buffer.from(`</script>\r\n`)
+        ]);
+        file.path = file.path.replace('.js', '.html')
+      }
+    }))
+    .pipe(dest('build'))
+}
+
 function buildClientJs (cb) {
-  const builtFiles = glob.sync(__dirname + '/src/pages/**/*.html')
+  const builtFiles = glob.sync(__dirname + '/build/pages/**/*.html')
   fs.writeFileSync('./build/client.json', JSON.stringify(arrToObj(builtFiles.map(f => shortid.generate()), builtFiles)))
   cb()
 }
 
-watch(['src/**/*.ts'], series(clean, buildTs, buildClientJs));
-exports.buildTs = buildTs;
-exports.default = series(clean, buildTs, buildClientJs);
+watch(['src/**/*.ts'], series(clean, buildTs, buildHtml, buildClientJs));
+exports.mergeSvelte = series(clean, mergeSvelte, buildClientJs);
+exports.default = series(clean, buildTs, buildHtml, buildClientJs);
