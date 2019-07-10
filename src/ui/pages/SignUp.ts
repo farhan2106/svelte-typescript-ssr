8eca3e0a96372e7de054fb413aaf01d7507127de
Farhan Ghazali <farhan_ghazali@yahoo.com>
2019-07-11 07:50:02 +0800

    Sign up

--- src/ui/pages/SignUp.ts
+++ src/ui/pages/SignUp.ts
@@ -1,84 +1,153 @@
+const Ajv = require('ajv');
+import { find } from 'lodash';
 import axios from 'axios';
-const Ajv = require('ajv')
-import { Machine, interpret, send } from 'xstate';
+import { Machine, interpret, send, assign } from 'xstate';
 import NavBar from './../components/NavBar/NavBar.html';NavBar;
 import signUpSchema from './../schemas/SignUp';
 
 const ajv = new Ajv({ allErrors: true });
 const validate = ajv.compile(signUpSchema);
 
+enum STATES {
+  DEFAULT = 'default',
+  FILLING = 'filling',
+  VALID = 'valid',
+  SUBMITTING = 'submitting',
+  SUBMITTED = 'submitted'
+}
+
+enum EVENTS {
+  FILL_FORM = 'FILL_FORM',
+  VALIDATE_FORM = 'VALIDATE_FORM',
+  SUBMIT_FORM = 'SUBMIT_FORM'
+}
+
 const appMachine = Machine({
   id: 'signUpPage',
-  initial: 'default',
+  initial: STATES.DEFAULT,
+  strict: true,
   context: {
-    username: '',
-    email: '',
-    password: ''
+    data: {
+      username: '',
+      email: '',
+      password: ''
+    },
+    errors: []
   },
   states: {
-    'default': {
+    [STATES.DEFAULT]: {
       on: {
-        FILL_FORM: 'filling'
+        [EVENTS.FILL_FORM]: STATES.FILLING
       }
     },
-    'filling': {
+    [STATES.FILLING]: {
       on: {
-        FILL_FORM: {
-          actions: ['fillForm', send('VALIDATE_FORM')]
+        [EVENTS.FILL_FORM]: {
+          actions: ['fillForm', send(EVENTS.VALIDATE_FORM)]
         },
-        VALIDATE_FORM: {
-          target: 'valid',
+        [EVENTS.VALIDATE_FORM]: {
+          target: STATES.VALID,
           cond: 'formValid'
         }
       }
     },
-    'valid': {
+    [STATES.VALID]: {
       on: {
-        SUBMIT_FORM: 'submitted'
+        [EVENTS.SUBMIT_FORM]: {
+          target: STATES.SUBMITTING
+        },
+        [EVENTS.FILL_FORM]: {
+          target: STATES.FILLING,
+          actions: ['fillForm', send(EVENTS.VALIDATE_FORM)]
+        }
       }
     },
-    // 'submitted': {
-    //   final: true
-    // }
+    [STATES.SUBMITTING]: {
+      invoke: {
+        id: 'submitForm',
+        src: (context: any, event: any) => submitForm(context),
+        onDone: {
+          target: STATES.SUBMITTED
+        },
+        onError: {
+          target: STATES.VALID,
+          actions: assign({ errors: (context: any, event: any) => [event.data.message] })
+        }
+      } as any
+    },
+    [STATES.SUBMITTED]: {
+      type: 'final'
+    }
   }
 }, {
   actions: {
-    fillForm: (context: any, event: any) => {
+    fillForm: (context, event) => {
       if (event && event.payload && event.payload.username) {
-        context.username = event.payload.username;
+        context.data.username = event.payload.username;
       }
       if (event && event.payload && event.payload.email) {
-        context.email = event.payload.email;
+        context.data.email = event.payload.email;
       }
       if (event && event.payload && event.payload.password) {
-        context.password = event.payload.password;
+        context.data.password = event.payload.password;
       }
     }
   },
   guards: {
-    formValid: (context: any, event: any) => {
-      const validateStatus = validate(context);
-      console.log(validate.errors)
+    formValid: (context, event) => {
+      const validateStatus = validate(context.data);
+      let errors = validate.errors
+      if (errors === null) errors = []
+      context.errors = errors
       return validateStatus as boolean;
     }
   }
 })
 
-const intepreter = interpret(appMachine).onTransition((state: any) => console.log(state))
+const intepreter = interpret(appMachine)
+  .onTransition((state) => console.log(state.event.type, state.value, state.context))
 intepreter.start();
 
+// html vars
+let errors: any[] = []
+let canSubmitForm = false
+
+// reactive
+let errorsInput: any = {}
+
+intepreter.subscribe(state => {
+  errors = state.context.errors
+  if (state.value === STATES.VALID) {
+    canSubmitForm = true
+  } else {
+    canSubmitForm = false
+  }
+})
+
+// non-html functions
+const submitForm = (data: any) => {
+  return axios.post('/api/signup', data)
+}
+
+const getError = (arr: any, key: any) => {
+  return (find(arr, e => e.dataPath === `.${key}`) || {}).message || ''
+}
+
+$: {
+  errorsInput.username = getError(errors, 'username')
+  errorsInput.email = getError(errors, 'email')
+  errorsInput.password = getError(errors, 'password')
+}
+
+// html functions
 function signUpHandler (this: HTMLFormElement, e: KeyboardEvent) {
-  // return axios.post('/api/signup', {
-  //   username: (this.elements.namedItem('username') as HTMLInputElement).value,
-  //   email: (this.elements.namedItem('email') as HTMLInputElement).value,
-  //   password: (this.elements.namedItem('password') as HTMLInputElement).value
-  // })
+  intepreter.send(EVENTS.SUBMIT_FORM)
 }
 
 function fillForm (this: HTMLInputElement, e: KeyboardEvent) {
-  intepreter.send('FILL_FORM', {
+  intepreter.send(EVENTS.FILL_FORM, {
     payload: {
-      [this.name]: this.value
+      [this.name]: this.value || ''
     }
   })
 }
